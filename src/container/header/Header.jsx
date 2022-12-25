889d5c3756eacda6c9e8c90e7fc6b700a31ea90b
docteurdev <adjeoumar@gmail.com>
2022-12-26 00:20:14 +0100

    redesigning Header h1

--- src/container/header/Header.jsx
+++ src/container/header/Header.jsx
@@ -42,7 +42,7 @@ function Header() {
             <h1 className="header-text title-anim2 ">développeur front</h1>
           </div>
           <div className="title-anim-bx">
-            <p className="p-text title-anim3">
+            <p className="p-text title-anim3 text-red-500">
               Passionné par la conception et le développement de projets
               digitaux, je me suis tout naturellement tourné vers une carrière
               de développeur web après une formation en Comptabilité et Gestion
